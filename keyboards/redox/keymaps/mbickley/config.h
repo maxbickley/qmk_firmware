a68ad7f33c91c1b12399a2d265baa5b3e36b492f
Max Bickley <mbickley@salami.dynamic.pixar.com>
2019-07-22 13:05:43 -0700

    updating keymap with fixes

--- keyboards/redox/keymaps/mbickley/config.h
+++ keyboards/redox/keymaps/mbickley/config.h
@@ -17,6 +17,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
+#define ONESHOT_TAP_TOGGLE 5  /* Tapping this number of times holds the key until tapped once again. */
+#define ONESHOT_TIMEOUT 5000  /* Time (in ms) before the one shot key is released */
+
 /* Use I2C or Serial, not both */
 #define USE_SERIAL
 // #define USE_I2C
