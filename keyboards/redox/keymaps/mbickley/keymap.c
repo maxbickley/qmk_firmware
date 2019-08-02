fa589d6d0b4dad49f7d8ae965a07bbaaa4382aff
Max Bickley <maxbickley@gmail.com>
2019-08-02 10:45:05 -0700

    new layers

--- keyboards/redox/keymaps/mbickley/keymap.c
+++ keyboards/redox/keymaps/mbickley/keymap.c
@@ -15,11 +15,16 @@ extern keymap_config_t keymap_config;
 #define SPCLOWER LT(_LOWER, KC_SPACE) // Tap for Space, hold for Raise
 
 //CleanUp
-#define CT_LBRC LCTL_T(KC_LBRC) //Tap for Left Bracket, hold for Control
-#define OS_RBRC LGUI_T(KC_RBRC) //Tap for Right Bracket, hold for GUI/OS
+#define CT_MINS LCTL_T(KC_MINS) //Tap for Left Bracket, hold for Control
+#define CT_EQL LCTL_T(KC_EQL) //Tap for Right Bracket, hold for GUI/OS
 #define OS_ENT LGUI_T(KC_ENT) //Tap for Enter, hold for GUI/OS
 #define OS_DEL LGUI_T(KC_DEL) //Tap for Delete, hold for GUI/OS
 #define CT_UND LCTL_T(KC_UNDS) //Tap for underscore, hold for Control
+#define TERM LALT(LCTL(LSFT(KC_T))) //Open terminal
+#define WSLEFT LCTL(LALT(KC_LEFT)) //Move to workspace left
+#define WSUP LCTL(LALT(KC_UP)) //Move to workspace up
+#define WSDOWN LCTL(LALT(KC_DOWN)) //Move to workspace down
+#define WSRIGHT LCTL(LALT(KC_RIGHT)) //Move to workspace right
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
   LOWER,
@@ -32,43 +37,43 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_QWERTY] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                                           ┌────────┬────────┬────────┬────────┬────────┬────────┐
-     KC_GRV  ,KC_1    ,KC_2    ,KC_3    ,KC_4    ,KC_5    ,                                            KC_6    ,KC_7    ,KC_8    ,KC_9    ,KC_0    ,KC_ESC  ,
+     KC_GRV  ,KC_1    ,KC_2    ,KC_3    ,KC_4    ,KC_5    ,                                            KC_6    ,KC_7    ,KC_8    ,KC_9    ,KC_0    ,KC_BSPC ,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐                         ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_TAB  ,KC_Q    ,KC_W    ,KC_E    ,KC_R    ,KC_T    ,KC_ESC  ,                          KC_DEL  ,KC_Y    ,KC_U    ,KC_I    ,KC_O    ,KC_P    ,KC_EQL  ,
+     KC_TAB  ,KC_Q    ,KC_W    ,KC_E    ,KC_R    ,KC_T    ,KC_ESC  ,                         TERM     ,KC_Y    ,KC_U    ,KC_I    ,KC_O    ,KC_P    ,KC_BSLS ,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_PGUP ,KC_A    ,KC_S    ,KC_D    ,KC_F    ,KC_G    ,OS_DEL  ,                         OS_ENT   ,KC_H    ,KC_J    ,KC_K    ,KC_L    ,KC_SCLN ,KC_QUOT ,
+     KC_PGUP ,KC_A    ,KC_S    ,KC_D    ,KC_F    ,KC_G    ,KC_LCBR ,                         KC_RCBR  ,KC_H    ,KC_J    ,KC_K    ,KC_L    ,KC_SCLN ,KC_ENT  ,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┐       ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_PGDN ,KC_Z    ,KC_X    ,KC_C    ,KC_V    ,KC_B    ,KC_TAB ,  KC_ENT ,       KC_BSLS  ,KC_BSPC ,KC_N    ,KC_M    ,KC_COMM ,KC_DOT  ,KC_SLSH ,KC_PIPE ,
+     KC_DEL  ,KC_Z    ,KC_X    ,KC_C    ,KC_V    ,KC_B    ,KC_HOME ,KC_END  ,       KC_QUOT  ,KC_DQT  ,KC_N    ,KC_M    ,KC_COMM ,KC_DOT  ,KC_SLSH ,KC_UNDS ,
   //├────────┼────────┼────────┼────────┼────┬───┴────┬───┼────────┼────────┤       ├────────┼────────┼───┬────┴───┬────┼────────┼────────┼────────┼────────┤
-     KC_MINS ,KC_EQL  ,CT_LBRC ,OS_RBRC ,    KC_LALT  ,    SPCRAISE,KC_LSFT ,       KC_LSFT  ,SPCLOWER,   CT_UND    ,    KC_LEFT ,KC_DOWN ,KC_UP   ,KC_RGHT 
+     KC_MINS ,CT_EQL  ,KC_LGUI ,KC_SLSH ,    KC_LSFT  ,   SPCRAISE ,KC_LALT ,       KC_BSPC  ,SPCLOWER,   KC_RSFT  ,    KC_LEFT  ,KC_DOWN ,KC_UP   ,KC_RGHT 
   //└────────┴────────┴────────┴────────┘    └────────┘   └────────┴────────┘       └────────┴────────┘   └────────┘    └────────┴────────┴────────┴────────┘
   ),
 
   [_RAISE] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                                           ┌────────┬────────┬────────┬────────┬────────┬────────┐
-     _______ ,KC_F1   ,KC_F2   ,KC_F3   ,KC_F4   ,KC_F5   ,                                            KC_F6   ,KC_F7   ,KC_F8   ,KC_F9   ,KC_F10  ,_______ ,
+     _______ ,_______ ,_______ ,_______ ,_______ ,_______ ,                                            _______ ,_______ ,_______ ,_______ ,_______ ,KC_DEL  ,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐                         ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
      KC_INS  ,KC_6    ,KC_7    ,KC_8    ,KC_9    ,KC_0    ,_______ ,                          _______ ,_______ ,_______ ,_______ ,_______ ,_______ ,_______ ,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_HOME ,_______ ,_______ ,_______ ,_______ ,_______ ,KC_LBRC ,                          _______ ,_______ ,_______ ,_______ ,_______ ,_______ ,_______ ,
+     KC_HOME ,KC_F1   ,KC_F2   ,KC_F3   ,KC_F4   ,KC_F5   ,KC_LPRN ,                          _______ ,_______ ,_______ ,_______ ,_______ ,_______ ,_______ ,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┐       ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┤
      KC_END  ,_______ ,_______ ,_______ ,_______ ,_______ ,KC_PGUP ,KC_PGDN ,        _______ ,_______ ,_______ ,KC_MPLY ,KC_VOLD ,KC_VOLU ,KC_MUTE ,_______ ,
   //├────────┼────────┼────────┼────────┼────┬───┴────┬───┼────────┼────────┤       ├────────┼────────┼───┬────┴───┬────┼────────┼────────┼────────┼────────┤
-     KC_ASTR ,KC_SLSH ,KC_LCBR ,KC_RCBR ,  OSM(MOD_LCTL),  _______ ,_______ ,        _______ ,_______ , OSM(MOD_LALT),   _______ ,_______ ,_______ ,_______ 
+     KC_ENT  ,KC_ASTR ,KC_SLSH ,KC_PLUS ,  OSM(MOD_LCTL),  _______ ,_______ ,        _______ ,_______ , OSM(MOD_LALT),   WSLEFT  ,WSDOWN  ,WSUP    ,WSRIGHT 
   //└────────┴────────┴────────┴────────┘    └────────┘   └────────┴────────┘       └────────┴────────┘   └────────┘    └────────┴────────┴────────┴────────┘
   ),
 
   [_LOWER] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                                           ┌────────┬────────┬────────┬────────┬────────┬────────┐
-     _______ ,KC_F1   ,KC_F2   ,KC_F3   ,KC_F4   ,KC_F5   ,                                            KC_F6   ,KC_F7   ,KC_F8   ,KC_F9   ,KC_F10  ,KC_BSPC ,
+     _______ ,_______ ,_______ ,_______ ,_______ ,_______ ,                                            _______ ,_______ ,_______ ,_______ ,_______ ,KC_DEL  ,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐                         ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_CALC ,KC_6    ,KC_7    ,KC_8    ,KC_9    ,KC_0    ,_______ ,                          _______ ,KC_1    ,KC_2    ,KC_3    ,KC_4    ,KC_5    ,_______ ,
+     KC_CALC ,_______ ,_______ ,_______ ,_______ ,_______ ,_______ ,                          _______ ,KC_1    ,KC_2    ,KC_3    ,KC_4    ,KC_5    ,_______ ,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_WBAK ,_______ ,KC_UP   ,_______ ,_______ ,_______ ,KC_LBRC ,                          _______ ,_______ ,_______ ,_______ ,_______ ,_______ ,_______ ,
+     KC_WBAK ,_______ ,KC_UP   ,_______ ,_______ ,_______ ,_______ ,                          KC_RPRN ,KC_F6   ,KC_F7   ,KC_F8   ,KC_F9   ,KC_F10  ,_______ ,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┐       ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_WFWD ,KC_LEFT ,KC_DOWN ,KC_RIGHT,_______ ,_______ ,KC_PGUP ,KC_PGDN ,        _______ ,_______ ,_______ ,_______ ,_______ ,_______ ,_______ ,_______ ,
+     KC_WFWD ,KC_LEFT ,KC_DOWN ,KC_RIGHT,_______ ,_______ ,_______ ,_______ ,        _______ ,_______ ,_______ ,KC_MPLY ,KC_VOLD ,KC_VOLU ,KC_MUTE ,_______ ,
   //├────────┼────────┼────────┼────────┼────┬───┴────┬───┼────────┼────────┤       ├────────┼────────┼───┬────┴───┬────┼────────┼────────┼────────┼────────┤
-     _______ ,_______ ,_______ ,_______ ,  OSM(MOD_LCTL),  _______ ,_______ ,        _______ ,_______ , OSM(MOD_LALT),   _______ ,_______ ,_______ ,_______ 
+     _______ ,_______ ,KC_LBRC ,KC_RBRC ,  OSM(MOD_LCTL),  _______ ,_______ ,        _______ ,_______ , OSM(MOD_LALT),   _______ ,_______ ,_______ ,_______ 
   //└────────┴────────┴────────┴────────┘    └────────┘   └────────┴────────┘       └────────┴────────┘   └────────┘    └────────┴────────┴────────┴────────┘
   ),
 
