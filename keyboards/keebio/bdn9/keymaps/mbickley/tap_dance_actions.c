822ec06ba0fe6c983fe9c98dfa65b76ddf60ebde
Max <maxbickley@gmail.com>
2019-08-07 22:25:12 -0700

    Updating katana tapdance

--- keyboards/keebio/bdn9/keymaps/mbickley/tap_dance_actions.c
+++ keyboards/keebio/bdn9/keymaps/mbickley/tap_dance_actions.c
@@ -2,5 +2,5 @@
 qk_tap_dance_action_t tap_dance_actions[] = {
     [EQL_PLUS]  = ACTION_TAP_DANCE_DOUBLE(KC_EQL,  KC_PLUS),
     [SCLN_COLN] = ACTION_TAP_DANCE_FN_ADVANCED_TIME(NULL, scln_coln_finished,       scln_coln_reset,      DANCING_TERM), // Requires scln_coln
-    [KTNA_RENDER]  = ACTION_TAP_DANCE_DOUBLE(LCTL(LSFT(KC_P)), LCTL(KC_P)), // Tap once for Live Render, twice for Preview Render
+    [KTNA_RENDER]  = ACTION_TAP_DANCE_FN_ADVANCED_TIME(NULL, katana_render_finished,       katana_render_reset,      DANCING_TERM), // Requires tap_dance_katana
 };
